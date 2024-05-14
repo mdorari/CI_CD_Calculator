6488c69bd3e075f6c79925e6bada443ec5601b31
md <mehrdad.dorari@gmail.com>
2024-05-14 13:19:09 +1000

    firebase setup

--- app/src/main/java/com/mehrdad/cicdcalculator/MainActivity.kt
+++ app/src/main/java/com/mehrdad/cicdcalculator/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.mehrdad.cicdcalculator
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mehrdad.cicdcalculator.domain.ExpressionPart
-import com.mehrdad.cicdcalculator.domain.Operation
 import com.mehrdad.cicdcalculator.presentation.CalculatorScreen
 import com.mehrdad.cicdcalculator.ui.theme.CICDCalculatorTheme
 
